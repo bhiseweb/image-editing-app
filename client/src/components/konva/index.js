206213809c254aacbbe2659ad560c3f361533c74
Leena <leena.web11@gmail.com>
2019-08-19 19:22:52 +0530

    added scalling and rotation of image

--- client/src/components/konva/index.js
+++ client/src/components/konva/index.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
-import { Stage, Layer, Image } from 'react-konva';
+import { Stage, Layer, Image, Transformer } from 'react-konva';
 
 class KonvaImage extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      image: new window.Image()
+      image: new window.Image(),
+      x: 50,
+      y: 50,
+      selected: false
     }
+    this.trRef = React.createRef()
+    this.imageNode = React.createRef()
   }
 
   componentDidUpdate (prevprops) {
@@ -22,18 +27,41 @@ class KonvaImage extends Component {
     }
     newImage.src = image
   }
+
+  removeSelect = e => {
+    this.setState({ selected: false })
+  }
   
+  onSelect = e => {
+    this.setState({ selected: true }, () => {
+      this.trRef.current.setNode(this.imageNode.current);
+      this.trRef.current.getLayer().batchDraw();
+    })
+  }
+
   render() {
     return (
-      <Stage width={600} height={600}>
+      <Stage
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onMouseDown={e => {
+          // deselect when clicked on empty area
+          const clickedOnEmpty = e.target === e.target.getStage();
+          if (clickedOnEmpty) {
+            this.removeSelect()
+          }
+        }} >
         <Layer>
           <Image
+            x={this.state.x}
+            y={this.state.y}
+            name="image"
             image={this.state.image}
             draggable
-            ref={node => {
-              this.imageNode = node;
-            }}
+            ref={this.imageNode}
+            onClick={this.onSelect}
            />
+           { this.state.selected && <Transformer ref={this.trRef}/> }
         </Layer>
       </Stage>
     );
